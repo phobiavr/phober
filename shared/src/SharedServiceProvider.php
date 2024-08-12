639f7916425729351d6257d0be41abb11fe8ffc0
Abdukhaligov <hikmat.pou@gmail.com>
2024-08-12 14:33:15 +0400

    update: db_log -> db_shared (p.2)
    (cherry picked from commit 4e845789ee07e7e92d216aef2f1699485f07545a)

--- shared/src/SharedServiceProvider.php
+++ shared/src/SharedServiceProvider.php
@@ -51,7 +51,7 @@ class SharedServiceProvider extends ServiceProvider
             'driver' => env('DB_SHARED_CONNECTION', 'mysql'),
             'host' => env('DB_SHARED_HOST', '127.0.0.1'),
             'port' => env('DB_SHARED_PORT', '3306'),
-            'database' => env('DB_SHARED_DATABASE', 'phober_log'),
+            'database' => env('DB_SHARED_DATABASE', 'phober_shared'),
             'username' => env('DB_SHARED_USERNAME', 'forge'),
             'password' => env('DB_SHARED_PASSWORD', ''),
         ]);
