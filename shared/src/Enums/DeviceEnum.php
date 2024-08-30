09642f4d76cf7b0b945453feecae47f2287d66e4
Abdukhaligov <hikmat.pou@gmail.com>
2024-08-30 22:33:37 +0400

    add: DeviceEnum
    (cherry picked from commit cef79c41d9b8a32bc55b5fbeb957c24fee59dea2)

---
+++ shared/src/Enums/DeviceEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Shared\Enums;
+
+enum DeviceEnum: string {
+    case HTC = 'HTC';
+    case OCULUS = 'OCULUS';
+    case PS_VR = 'PS_VR';
+    case OMNI = 'OMNI';
+    case DOF_3 = 'DOF_3';
+}
