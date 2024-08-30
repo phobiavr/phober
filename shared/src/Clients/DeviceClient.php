1a1e8701701bd4f66ffc12c6b930f33331744612
Abdukhaligov <hikmat.pou@gmail.com>
2024-08-30 05:51:04 +0400

    add: Session & update schedule

---
+++ shared/src/Clients/DeviceClient.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Shared\Clients;
+
+use DateTime;
+use GuzzleHttp\Promise\PromiseInterface;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+use Shared\Device\ScheduleEnum;
+
+class DeviceClient {
+    protected static ?string $url = 'http://device-service';
+
+    public static function schedule(ScheduleEnum $type, int $instanceId, DateTime $start, DateTime $end = null): PromiseInterface|Response|bool {
+        return Http::accept('application/json')->withHeaders([
+            'Authorization' => 'Bearer ' . request()->bearerToken()
+        ])->post(self::$url . '/schedule', [
+            "type"        => $type->value,
+            "instance_id" => $instanceId,
+            "start"       => $start->format('Y-m-d H:i:s'),
+            "end"         => $end?->format('Y-m-d H:i:s')
+        ]);
+    }
+}
