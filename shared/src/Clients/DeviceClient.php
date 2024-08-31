699dd4367e1e121b84fb5b47cc0db3937121bb31
Abdukhaligov <hikmat.pou@gmail.com>
2024-09-01 03:14:26 +0400

    add: pricing
    (cherry picked from commit f41973e44747b8dfdf9ebc64b2356281b0e266ae)

--- shared/src/Clients/DeviceClient.php
+++ shared/src/Clients/DeviceClient.php
@@ -7,11 +7,13 @@ use GuzzleHttp\Promise\PromiseInterface;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 use Shared\Enums\ScheduleEnum;
+use Shared\Enums\SessionTariffEnum;
+use Shared\Enums\SessionTimeEnum;
 
 class DeviceClient {
     protected static ?string $url = 'http://device-service';
 
-    public static function schedule(ScheduleEnum $type, int $instanceId, DateTime $start, DateTime $end = null): PromiseInterface|Response|bool {
+    public static function schedule(ScheduleEnum $type, int $instanceId, DateTime $start, DateTime $end = null): PromiseInterface|Response {
         return Http::accept('application/json')->withHeaders([
             'Authorization' => 'Bearer ' . request()->bearerToken()
         ])->post(self::$url . '/schedule', [
@@ -19,6 +21,14 @@ class DeviceClient {
             "instance_id" => $instanceId,
             "start"       => $start->format('Y-m-d H:i:s'),
             "end"         => $end?->format('Y-m-d H:i:s')
+        ]);
+    }
+
+    public static function price(int $instanceId, SessionTariffEnum $tariff, SessionTimeEnum $time): PromiseInterface|Response {
+        return Http::accept('application/json')->post(self::$url . '/price', [
+            "instance_id" => $instanceId,
+            "tariff"      => $tariff->value,
+            "time"        => $time->value,
         ]);
     }
 }
