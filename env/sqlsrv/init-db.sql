46342d650432bc8b58f7c9ad259ea7322eaa35e4
Hikmat Abdukhaligov <hikmat.pou@gmail.com>
2024-09-15 14:49:31 +0400

    add: sqlite for auth-server

--- env/sqlsrv/init-db.sql
+++ env/sqlsrv/init-db.sql
@@ -2,3 +2,8 @@ IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'phober_staff')
 BEGIN
 CREATE DATABASE phober_staff;
 END
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'phober_auth')
+    BEGIN
+        CREATE DATABASE phober_auth;
+    END
