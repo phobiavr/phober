78c7240c836a9d85bec7939cc9c323ae67ac40d5
Hikmat Abdukhaligov <hikmat.pou@gmail.com>
2024-09-24 04:35:46 +0400

    add: laravel-common package

--- shared/src/Enums/InvoicePaymentMethodEnum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Shared\Enums;
-
-enum InvoicePaymentMethodEnum: string {
-    case CASH = 'CASH';
-    case CARD = 'CARD';
-    case BONUS = 'BONUS';
-}
--- shared/src/Enums/InvoiceStatusEnum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Shared\Enums;
-
-enum InvoiceStatusEnum: string {
-    case QUEUE = 'QUEUE';
-    case PAYED = 'PAYED';
-    case CANCELED = 'CANCELED';
-}
